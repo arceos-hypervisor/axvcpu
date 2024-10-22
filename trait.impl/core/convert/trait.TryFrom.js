f339dc8e9e828a1fd44263ead117d55b31d4b9ae
hky1999 <976929993@qq.com>
2024-10-22 16:06:57 +0000

    Deploying to gh-pages from @ arceos-hypervisor/axvcpu@b98ebf3b685a27b76583059037be1c25600803fa 🚀

---
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["axvcpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"enum\" href=\"axvcpu/enum.AccessWidth.html\" title=\"enum axvcpu::AccessWidth\">AccessWidth</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[385]}
