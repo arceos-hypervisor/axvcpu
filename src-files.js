6f8a255aa19c44758c717b626a828a744176efb9
hky1999 <976929993@qq.com>
2025-01-02 07:47:37 +0000

    Deploying to gh-pages from @ arceos-hypervisor/axvcpu@8414a575723f929d5fd24010ce16446d90ecf268 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["axvcpu",["",[],["arch_vcpu.rs","exit.rs","hal.rs","lib.rs","percpu.rs","vcpu.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[85]}
---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["axvcpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"axvcpu/enum.AccessWidth.html\" title=\"enum axvcpu::AccessWidth\">AccessWidth</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[376]}
