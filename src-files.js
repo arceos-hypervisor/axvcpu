1df702c8fb7afbf017fbf021a1f90df9165a08be
aarkegz <aarkegz@gmail.com>
2025-08-15 07:29:09 +0000

    Deploying to gh-pages from @ arceos-hypervisor/axvcpu@6364b7b13f0e4efb969295d07667215e79969a1d 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["axvcpu",["",[],["arch_vcpu.rs","exit.rs","hal.rs","lib.rs","percpu.rs","test.rs","vcpu.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[95]}
